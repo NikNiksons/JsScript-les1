400e79b2bd92eb260b085567cc3d132fa6e2c9ee
NikNiksons <123307796+NikNiksons@users.noreply.github.com>
2023-02-19 19:52:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let city = 'Київ';
+city = 'Львів';
+let address = city;
+alert(address);
+let n = 10,
+    result = n * 100;
+console.log(result);
+
+let pen = 4,
+    costPen = 5.25,
+    pensil = 6,
+    costPensil = 3.50,
+    hrn = 'грн',
+    sum = pen * costPen + pensil * costPensil + hrn;
+document.write(sum);
